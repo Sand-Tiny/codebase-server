154f8d6edfc2df60e4ac7a689a54d4eb71dc63a9
liuxiaobo <liuxiaobo@haixue.com>
2020-06-23 14:50:12 +0800

    提交

---
+++ codebase-client/src/test/java/com/tiny/codebase/CodebaseApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tiny.codebase;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
