645416aaa73e21cc4786081b711370066ef3d863
liuxiaobo <liuxiaobo@haixue.com>
2020-06-24 13:59:39 +0800

    添加自动注册

--- codebase-client/src/main/java/com/tiny/codebase/CodebaseApplication.java
+++ codebase-client/src/main/java/com/tiny/codebase/CodebaseApplication.java
@@ -1,11 +1,10 @@
 package com.tiny.codebase;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableAutoConfiguration
 @SpringBootApplication
-@EnableDubboConfig
 public class CodebaseApplication {
 
 	public static void main(String[] args) {
--- codebase-service/src/main/java/com/tiny/codebase/CodebaseApplication.java
+++ codebase-service/src/main/java/com/tiny/codebase/CodebaseApplication.java
@@ -1,11 +1,11 @@
 package com.tiny.codebase;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableDubboConfig
+@EnableAutoConfiguration
 public class CodebaseApplication {
 
 	public static void main(String[] args) {
