591fbbc2c24e734e850d5bb41440e001c760e7a6
liuxiaobo <liuxiaobo@haixue.com>
2020-06-30 17:40:49 +0800

    导包问题

--- codebase-client/src/main/java/com/tiny/codebase/controller/CustomerLotteryController.java
+++ codebase-client/src/main/java/com/tiny/codebase/controller/CustomerLotteryController.java
@@ -2,11 +2,16 @@ package com.tiny.codebase.controller;
 
 import javax.annotation.Resource;
 
+import com.tiny.codebase.entity.CustomerLottery;
 import com.tiny.codebase.service.CustomerLotteryService;
 
 public class CustomerLotteryController {
     
     @Resource
     private CustomerLotteryService customerLotteryService;
+    
+    public CustomerLottery get(long customerLotteryId) {
+        return customerLotteryService.get(customerLotteryId);
+    }
 
 }
