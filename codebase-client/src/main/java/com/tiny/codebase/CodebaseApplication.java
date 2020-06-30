01952ba9de585a9c1a89789fd3130ae92dfd1ac5
liuxiaobo <liuxiaobo@haixue.com>
2020-06-30 18:56:21 +0800

    client配置

--- codebase-client/src/main/java/com/tiny/codebase/CodebaseApplication.java
+++ codebase-client/src/main/java/com/tiny/codebase/CodebaseApplication.java
@@ -1,10 +1,12 @@
 package com.tiny.codebase;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableAutoConfiguration
 @SpringBootApplication
+@EnableDubboConfig
 public class CodebaseApplication {
 
 	public static void main(String[] args) {
--- codebase-client/src/main/java/com/tiny/codebase/controller/CustomerLotteryController.java
+++ codebase-client/src/main/java/com/tiny/codebase/controller/CustomerLotteryController.java
@@ -1,15 +1,26 @@
 package com.tiny.codebase.controller;
 
-import javax.annotation.Resource;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.tiny.codebase.entity.CustomerLottery;
 import com.tiny.codebase.service.CustomerLotteryService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(value = "用户奖品", tags = "用户奖品", description = "用户奖品")
+@RequestMapping("customerLottery")
+@RestController
 public class CustomerLotteryController {
-    
-    @Resource
+
+    @DubboReference(version = "1.0.0", check = false)
     private CustomerLotteryService customerLotteryService;
-    
+
+    @GetMapping("get")
+    @ApiOperation(value = "查询")
     public CustomerLottery get(long customerLotteryId) {
         return customerLotteryService.get(customerLotteryId);
     }
