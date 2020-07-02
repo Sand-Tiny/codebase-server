d3a09269b10e419067eb855c9fb053275b336ba0
liuxiaobo <liuxiaobo@haixue.com>
2020-07-02 18:07:47 +0800

    flow pass

--- codebase-service/src/main/java/com/tiny/codebase/CodebaseApplication.java
+++ codebase-service/src/main/java/com/tiny/codebase/CodebaseApplication.java
@@ -1,11 +1,15 @@
 package com.tiny.codebase;
 
+import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@DubboComponentScan(basePackages="com.tiny.codebase.service")
+@MapperScan(basePackages="com.tiny.codebase.dao")
 public class CodebaseApplication {
 
 	public static void main(String[] args) {
